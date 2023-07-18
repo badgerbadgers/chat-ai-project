135e0a366f5b3ace2515317535b801f01a6925a4
badgerbadgers <tim.almacen@gmail.com>
2023-07-17 19:38:12 -0700

    working version of chat-gpt clone using chat-gpt3.5 model

---
+++ app/page.tsx
@@ -0,0 +1,312 @@
+// // import Image from 'next/image'
+
+// // export default function Home() {
+// //   return (
+// //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+// //       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+// //         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+// //           Get started by editing&nbsp;
+// //           <code className="font-mono font-bold">app/page.tsx</code>
+// //         </p>
+// //         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+// //           <a
+// //             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+// //             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+// //             target="_blank"
+// //             rel="noopener noreferrer"
+// //           >
+// //             By{' '}
+// //             <Image
+// //               src="/vercel.svg"
+// //               alt="Vercel Logo"
+// //               className="dark:invert"
+// //               width={100}
+// //               height={24}
+// //               priority
+// //             />
+// //           </a>
+// //         </div>
+// //       </div>
+
+// //       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+// //         <Image
+// //           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+// //           src="/next.svg"
+// //           alt="Next.js Logo"
+// //           width={180}
+// //           height={37}
+// //           priority
+// //         />
+// //       </div>
+
+// //       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+// //         <a
+// //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           <h2 className={`mb-3 text-2xl font-semibold`}>
+// //             Docs{' '}
+// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+// //               -&gt;
+// //             </span>
+// //           </h2>
+// //           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+// //             Find in-depth information about Next.js features and API.
+// //           </p>
+// //         </a>
+
+// //         <a
+// //           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           <h2 className={`mb-3 text-2xl font-semibold`}>
+// //             Learn{' '}
+// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+// //               -&gt;
+// //             </span>
+// //           </h2>
+// //           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+// //             Learn about Next.js in an interactive course with&nbsp;quizzes!
+// //           </p>
+// //         </a>
+
+// //         <a
+// //           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           <h2 className={`mb-3 text-2xl font-semibold`}>
+// //             Templates{' '}
+// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+// //               -&gt;
+// //             </span>
+// //           </h2>
+// //           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+// //             Explore the Next.js 13 playground.
+// //           </p>
+// //         </a>
+
+// //         <a
+// //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+// //           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           <h2 className={`mb-3 text-2xl font-semibold`}>
+// //             Deploy{' '}
+// //             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+// //               -&gt;
+// //             </span>
+// //           </h2>
+// //           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+// //             Instantly deploy your Next.js site to a shareable URL with Vercel.
+// //           </p>
+// //         </a>
+// //       </div>
+// //     </main>
+// //   )
+// // }
+// import { NextPage } from "next";
+// import Head from "next/head";
+// import Link from "next/link";
+// import React from "react";
+
+// const HomePage: NextPage = () => {
+//   return (
+//     <>
+//     <Head>
+//       <title>OpenAI API Starter</title>
+//       <meta name="description" content="" />
+//       <link rel="icon" href="/favicon.ico" />
+//     </Head>
+//     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+//       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+//         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+//         Astro <span className="text-[hsl(280,100%,70%)]">AI</span>
+//         </h1>
+//         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+//           <Link
+//             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+//             href="/review"
+//           >
+//             <h3 className="text-2xl font-bold">Product Reviews →</h3>
+//             <div className="text-lg">
+//               Generate purpose-built product reviews.
+//             </div>
+//           </Link>
+//           <Link
+//             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+//             href="/short-story"
+      
+//           >
+//             <h3 className="text-2xl font-bold">Short Story →</h3>
+//             <div className="text-lg">
+// Create a 2-sentence horror story.
+//             </div>
+//           </Link>
+//           <Link
+//             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+//             href="/test"
+      
+//           >
+//             <h3 className="text-2xl font-bold">test →</h3>
+//             <div className="text-lg">
+// Create a test
+//             </div>
+//           </Link>
+//         </div>
+       
+//       </div>
+//     </main>
+//   </>
+//   );
+// };
+
+// export default HomePage;
+
+'use client'; // This is a client component
+import { NextPage } from "next";
+import Head from "next/head";
+// import Link from "next/link";
+// import hljs from "highlight.js";
+import { useState, useRef, useEffect } from "react";
+import React from "react";
+
+const HomePage: NextPage = () => {
+  // Create a ref for the div element
+  const textDivRef = useRef<HTMLDivElement>(null);
+  const [productInput, setProductInput] = useState("");
+  const [result, setResult] = useState(() => "");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Add a click event listener to the copy icon that copies the text in the div to the clipboard when clicked
+  useEffect(() => {
+    const copyIcon = document.querySelector(".copy-icon");
+    if (!copyIcon) return;
+    copyIcon.addEventListener("click", () => {
+      const textDiv = textDivRef.current;
+      let text;
+      if (textDivRef.current) {
+        text = textDivRef.current.textContent;
+      }
+      // Create a hidden textarea element
+      const textArea: HTMLTextAreaElement = document.createElement("textarea"); // Define type as HTMLTextAreaElement
+      document.body.appendChild(textArea);
+
+      // Select the text in the textarea
+      textArea.select();
+
+      // Copy the text to the clipboard
+      document.execCommand("copy");
+
+      // Remove the textarea element
+      document.body.removeChild(textArea);
+    });
+  }, []);
+
+
+  async function onSubmit(event: { preventDefault: () => void; }) {
+    event.preventDefault();
+    setIsLoading(true);
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product: productInput }),
+    })
+    const data = await response.json();
+    console.log("data", data);
+    // console.log("data.result", data.result);
+
+    let rawResult = data.item;
+
+    // console.log("rawResult");
+
+    // set result to the highlighted code. Address this error: Argument of type 'string' is not assignable to parameter of type '(prevState: undefined) => undefined'.ts(2345)
+    setResult(rawResult);
+
+    setProductInput("");
+    setIsLoading(false);
+  }
+
+  return (
+    <div>
+       <Head>
+      <title>#Facts</title>
+      <meta name="description" content="" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+      <main
+        className="flex flex-col 
+                    items-center justify-center m-20"
+      >
+        <h3 className="text-slate-900 text-xl mb-3">
+          #Facts
+        </h3>
+        <p className="text-slate-700 text-lg mb-3">
+          Enter a topic, any topic you want to chat about.{" "}
+        </p>
+        <form onSubmit={onSubmit}>
+          <input
+            className="text-sm text-gray-base w-full 
+                              mr-3 py-5 px-4 h-2 border 
+                              border-gray-200 rounded mb-2"
+            type="text"
+            name="product"
+            placeholder="Enter anything ..."
+            value={productInput}
+            onChange={(e) => setProductInput(e.target.value)}
+          />
+
+          <button
+            className="text-sm w-full bg-fuchsia-600 h-7 text-white
+                              rounded-2xl mb-10"
+            type="submit"
+          >
+            Generate Story
+          </button>
+        </form>
+        {isLoading ? (
+          <p>Loading... be patient.. may take 30s+</p>
+        ) : result ? (
+          <div className="relative w-2/4 ">
+            <div className="rounded-md border-spacing-2 border-slate-900 bg-slate-100 break-words max-w-500 overflow-x-auto  ">
+              <div
+                ref={textDivRef}
+                className="m-5 "
+                dangerouslySetInnerHTML={{ __html: result }}
+              />
+            </div>
+            <div className="copy-icon absolute top-0 right-0 mt-2 mr-2 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-copy"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <rect x="8" y="8" width="12" height="12" rx="2"></rect>
+                <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+              </svg>
+            </div>
+          </div>
+        ) : null}
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
